d78ce3a404c3e3d8ed84429f69fe5381e77b4bd8
ddanelynn <dingyoujia0823@gmail.com>
2022-01-09 21:16:52 +0800

    visa OA question

---
+++ NumberAddition.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+// 6, 7, 9, 8, 6
+// sort
+// 6, 6, 7, 8, 9
+// add diff
+// 9, 9, 10, 11, 9     +3
+// rearrange
+// 9, 9, 9, 10, 11
+
+// 11, 11, 11, 11, 12
+// 12, 12, 12, 12, 12
+
+
+public class NumberAddition {
+    public static int countAddtion(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        while (nums[nums.length - 1] > nums[0]) {
+            int diff = nums[nums.length - 1] - nums[0];
+            count += diff;
+            int temp = nums[nums.length - 1];
+            int[] newArr = new int[nums.length];
+            for (int i = 0; i < nums.length - 1; i++) {
+                newArr[i] = nums[i] + diff;
+            }
+            for (int i = 1; i < nums.length; i++) {
+                nums[i] = newArr[i - 1];
+            }
+            nums[0] = temp;
+            // for (int i = 0; i < nums.length; i++) {
+            //     System.out.print(nums[i] + " ");
+                
+            // }
+            // System.out.println();
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countAddtion(new int[]{5, 6, 8, 8, 5}));
+    }
+}
