c47698cfe7b4dc73a3157f56391537eb1ef922fe
ddanelynn <dingyoujia0823@gmail.com>
2022-09-26 10:21:26 +0800

    divde and conquer for max subarray

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,5 +1,5 @@
 public class MaxSubArray {
-    public int maxSubArray(int[] nums) {
+    public static int maxSubArray(int[] nums) {
         int currMax = nums[0];
         int currSum = 0;
 
@@ -12,4 +12,45 @@ public class MaxSubArray {
         }
         return currMax;
     }
+
+    // divice and conquer method: O(nlogn)
+    public static int[] maxSum(int[] nums, int left, int right) {
+        if (left == right) {
+            return new int[] { nums[left], left, right };
+        }
+        int mid = left + (right - left) / 2;
+        int[] leftResult = maxSum(nums, left, mid);
+        int[] rightResult = maxSum(nums, mid + 1, right);
+        int leftMax = leftResult[0];
+        int rightMax = rightResult[0];
+
+        if (leftMax > 0 && rightMax > 0) {
+            int sum = arrSum(nums, leftResult[1], rightResult[2]);
+            if (sum > Math.max(leftMax, rightMax)) {
+                return new int[] { sum, leftResult[1], rightResult[2] };
+            }
+        }
+
+        if (leftMax > rightMax) {
+            return new int[] { leftMax, leftResult[1], leftResult[2] };
+        } else {
+            return new int[] { rightMax, rightResult[1], rightResult[2] };
+        }
+
+    }
+
+    public static int arrSum(int[] nums, int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { -5, 4, 3, -3, -10, -5, 1 };
+        System.out.println(maxSum(nums, 0, nums.length - 1)[0]);
+        System.out.println(maxSum(nums, 0, nums.length - 1)[0] == maxSubArray(nums));
+    }
+
 }
